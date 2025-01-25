34672d13ffb5b08c94795305b99ff3adb7391863
tore gard <dragerot@gmail.com>
2025-01-25 18:59:07 +0100

    Initial commit

---
+++ src/main/kotlin/net/toregard/neo4jgrapdbspeingboot3kotlin/Neo4jgrapdbspeingboot3kotlinApplication.kt
@@ -0,0 +1,11 @@
+package net.toregard.neo4jgrapdbspeingboot3kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Neo4jgrapdbspeingboot3kotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<Neo4jgrapdbspeingboot3kotlinApplication>(*args)
+}
---
+++ src/test/kotlin/net/toregard/neo4jgrapdbspeingboot3kotlin/Neo4jgrapdbspeingboot3kotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package net.toregard.neo4jgrapdbspeingboot3kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Neo4jgrapdbspeingboot3kotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
